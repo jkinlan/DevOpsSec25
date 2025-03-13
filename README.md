5237c762542d357cd035ca77a5e15bd4895f251b
jkinlan <jkinlan@kravemedia.ie>
2025-03-13 21:00:06 +0000

    Step 16
    Commit the readme file and push your readme.md file to GitHub

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# DevOpsSec25
+# Week 2 Lab – Bash & Git
+James Kinlan - 24172901
+[DevOpsSec25](https://github.com/jkinlan/DevOpsSec25)
+---
+To run the script.sh run: `bash script.sh`
--- devops-lab-2-task/script.sh
+++ devops-lab-2-task/script.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
 
+#step 8
 rm -rf fol_1
 rm -rf fol_2
 
+#step 3
 mkdir fol_1
 mkdir fol_2
 
+#step 3
 touch fol_1/1_1.txt
 touch fol_1/1_2.txt
 touch fol_1/1_3.txt
+
+#step 3
 touch fol_2/2_1.txt
 touch fol_2/2_2.txt
 touch fol_2/2_3.txt
+
+#step 10 - not working in windows
+chmod 600 fol_1/*_1.txt
+chmod 600 fol_1/*_3.txt
+chmod 600 fol_2/*_1.txt
+chmod 600 fol_2/*_3.txt
+
+#step 11 - not working in windows
+chmod 600 fol_1/*_2.txt
+chmod 600 fol_2/*_2.txt
